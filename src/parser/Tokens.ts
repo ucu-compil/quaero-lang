e4a2828d14dee56861023d7016d2a59621231e02
Max <max@biller.uy>
2017-10-17 19:38:09 -0300

    Tokens string

--- src/parser/Tokens.ts
+++ src/parser/Tokens.ts
@@ -34,6 +34,7 @@ export const tokens = {
   float:        { match: /[0-9]+\.[0-9]+(?:[eE][-+]?[0-9]+)?/, value: (x: string) => (parseFloat(x)) },
   hex:          { match: /0[xX][0-9a-f-A-F]+/, value: (x: string) => (parseInt(x,16)) },
   integer:      { match: /[0-9]+/, value: (x: string) => (parseFloat(x)) },
+  string:       { match: /"(?:[^"\\]|\\.)*"/, value: (x: string) => (x) },
 
   // Identifiers
   identifier:   /[a-zA-Z_][a-zA-Z0-9_]*/,
